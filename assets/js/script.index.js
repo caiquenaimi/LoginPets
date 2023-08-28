15e7862b2c76f360b06952fe576bef2aa61d688c
Caique <caiquenaimi.564@gmail.com>
2023-08-28 08:07:36 -0300

    feat verirfyInputs function

--- assets/js/script.index.js
+++ assets/js/script.index.js
@@ -19,3 +19,68 @@ class Pet {
 
 }
 
+class PetList {
+    constructor() {
+        this.pets = [];
+    }
+
+    add(param) {
+        if (verifyInputs()) {
+            return;
+        }
+        else {
+            this.pets.push(param);
+        }
+    }
+
+}
+
+const petList = new PetList();
+
+function verifyInputs() {
+    let tutorName = document.getElementById("tutorName").value;
+    let petName = document.getElementById("petName").value;
+    let species = document.getElementById("species").value;
+    let image = document.getElementById("image").value;
+    let date = document.getElementById("date").value;
+
+    let flag = false;
+
+    if (tutorName == "") {
+        sendMsg("Campo obrigatório", "error", "tutorName");
+        flag = true;
+    } else {
+        sendMsg("", "", "name");
+    }
+
+    if (petName == "") {
+        sendMsg("Campo obrigatório", "error", "petName");
+        flag = true;
+    } else {
+        sendMsg("", "", "petName");
+    }
+
+    if (species == "") {
+        sendMsg("Campo obrigatório", "error", "species");
+        flag = true;
+    } else {
+        sendMsg("", "", "species");
+    }
+
+    if (image == "") {
+        sendMsg("Campo obrigatório", "error", "image");
+        flag = true;
+    } else {
+        sendMsg("", "", "image");
+    }
+
+    if (date == "") {
+        sendMsg("Campo obrigatório", "error", "date");
+        flag = true;
+    }
+    else {
+        sendMsg("", "", "date");
+    }
+
+    return flag;
+}
